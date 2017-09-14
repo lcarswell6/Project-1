6a0ac3eb5116521e7aea77224265f19d899217f0
lcarswell6 <lcarswell6@gmail.com>
2017-09-14 13:19:57 -0400

    added deal function

--- blackjack.js
+++ blackjack.js
@@ -1,16 +1,19 @@
-//dom elements
-/*var hit = document.getElementById("hitBtn")
+var hit = document.getElementById("hitBtn")
 var stay = document.getElementById("stayBtn")
 var deal = document.getElementById("dealBtn")
 var resetHand = document.getElementById("resetHandBtn")
 var resetWins = document.getElementById("resetWinsBtn")
-var login = document.getElementById("logInBtn")
-var logout = document.getElementById("logOutBtn")*/
+var playerHand = [];
+var dealerHand = [];
+var playerTotal = 0;
+var dealerTotal = 0;
+
+
 
 // card variables to build deck
 var deck = newArray();
 var suits = ["spades", "diamonds", "clubs", "hearts"];
-var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
 //function to create deck 
 function createDeck() {
@@ -18,36 +21,21 @@ function createDeck() {
     for (var i = 0; i < values.length; i++) {
         for (var j = 0; j < suits.length; j++) {
             var weight = parseInt(values[i]);
-            if (values[i] == "J" || "Q" || "K")
+            if (values[i] === "J" || "Q" || "K")
                 weight = 10;
-            if (values[i] == "A")
-                weight == 11;
+            if (values[i] === "A")
+                weight = 1;
             var card = { Value: values[i], Suit: suits[j], Weight: weight };
             deck.push(card);
         }
     }
 }
 
-//players
-var players = new Array();
-function createPlayers(num) {
-    players = new Array();
-    for (var i = 1; i <= num; i++) {
-        var hand = new Array();
-        var player = { Name: 'Player' + i, ID: i, Points: 0, Hand: hand };
-        players.push(player);
-    }
-}
-
-//deal 
-function dealHands() {
-    for (var i = 0; i < 2; i++) {
-        for (var x = 0; x < players.length; x++) {
-            var card = deck.pop();
-            players[x].Hand.push(card);
-            renderCard(card, x);
-            updatePoints();
+function deal() {
+        for (let i = 0; i < 2; i++) {
+            playerHand.push(deck[Math.floor(Math.random() * (deck.length - 0)) + 0]);
+            
+            dealerHand.push(deck[Math.floor(Math.random() * (deck.length - 0)) + 0]);
         }
-    }
-    updateDeck();
-}
+        console.log("Here's your hand: " + JSON.stringify(playerHand));
+        console.log("Dealer's hand: " + JSON.stringify(dealerHand));
